be98a223d0e5fe0c5074a249f71038db60cb6cd8
Galaxeo <hwjustincheok@gmail.com>
2024-03-27 17:58:14 -0400

    feat: add animation for nav bar items

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,41 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./App.css";
+import { easing } from "maath";
 
 function Overlay() {
   {
     /**/
   }
   const [expandedItem, setExpandedItem] = useState<string | null>(null);
-  const [menuWidth, setMenuWidth] = useState(200);
-  // const oldflexRatios = (expandedItem: string | null) => { flex: expandedItem ? 1 : 0.5 };
-  // const [flexRatios, setFlexRatios] = useState(oldflexRatios)
+  const [menuWidth, setMenuWidth] = useState(300);
+  const ref = useRef(null);
+
   const handleExpandedItem = (itemName: string) => {
+    const menu = ref.current as HTMLElement;
     if (expandedItem === itemName) {
       setExpandedItem(null);
-      setMenuWidth(200);
-      // flexRatios(expandedItem);
-    } else {
-      setExpandedItem(itemName);
       setMenuWidth(300);
-      // flexRatios(expandedItem);
+      menu.style.gridTemplateColumns = "1fr 1fr 1fr";
+    } else {
+      if (itemName === "about") {
+        menu.style.gridTemplateColumns = "2fr 1fr 1fr";
+      }
+      if (itemName === "keyboards") {
+        menu.style.gridTemplateColumns = "1fr 2fr 1fr";
+      }
+      if (itemName === "works") {
+        menu.style.gridTemplateColumns = "1fr 1fr 2fr";
+      }
+      setExpandedItem(itemName);
+      setMenuWidth(325);
     }
   };
+  // function to add className to element
+  const menuItemClass = (itemName: string) =>
+    itemName === expandedItem ? "scaled" : "";
+
+  useEffect(() => {});
+
   return (
     <>
       <div
@@ -34,11 +50,31 @@ function Overlay() {
         cheok.works
       </div>
       <div>
-        <div className="topMenu" style={{ width: `${menuWidth}px` }}>
-          {/*top has keyboards, about, works in a menu clickable*/}
-          <p onClick={() => handleExpandedItem("keyboards")}>keyboards</p>
-          <p onClick={() => handleExpandedItem("about")}>about</p>
-          <p onClick={() => handleExpandedItem("works")}>works</p>
+        <div ref={ref} className="topMenu" style={{ width: `${menuWidth}px` }}>
+          <p
+            className={menuItemClass("about")}
+            onClick={() => {
+              handleExpandedItem("about");
+            }}
+          >
+            about
+          </p>
+          <p
+            className={menuItemClass("keyboards")}
+            onClick={() => {
+              handleExpandedItem("keyboards");
+            }}
+          >
+            keyboards
+          </p>
+          <p
+            className={menuItemClass("works")}
+            onClick={() => {
+              handleExpandedItem("works");
+            }}
+          >
+            works
+          </p>
         </div>
         <hr className="topMenuHr"></hr>
       </div>
